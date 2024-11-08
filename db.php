677dc376596378f52761d10f96c780ca3ff1ceb9
JanSeth1 <janseth@duck.com>
2024-11-08 20:51:04 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,24 +1,11 @@
 <?php
-$host = 'bqryt896i0vmg4w1qquw-mysql.services.clever-cloud.com';
-$dbname = 'bqryt896i0vmg4w1qquw';
-$username = 'u5bbeglcixrwa0jc';
-$password = 'iVYr5zqUNf3A3gLBgy5N';
-$port = 3306;
+$servername = "whpeu.h.filess.io";
+$username = "portfoliodatabase_sentmetthy";
+$password = "84b7545529089cca782230034da28d547976fff7";
+$dbname = "portfoliodatabase_sentmetthy";
 
-// Set DSN (Data Source Name)
-$dsn = "mysql:host=$host;dbname=$dbname;port=$port";
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-try {
-    // Create a PDO instance (connect to the database)
-    $pdo = new PDO($dsn, $username, $password);
-    
-    // Set the PDO error mode to exception
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-    // If the connection is successful
-    echo "Connected successfully";
-} catch (PDOException $e) {
-    // If there is an error during connection
-    echo "Connection failed: " . $e->getMessage();
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
-?>
