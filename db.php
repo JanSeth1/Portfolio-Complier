46458e1e220215189941f50c9aa884bcbe2fb4b5
JanSeth1 <janseth@duck.com>
2024-12-09 02:41:57 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,10 +1,10 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "portfoliodatabase";
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$password = getenv('DB_PASS');
+$dbname = getenv('DB_NAME');
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($host, $user, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
